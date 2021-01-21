ab2150a850178ba9e52dc6d63317bcb156b49874
NormanFrieman <ezequiel-d.s@hotmail.com>
2021-01-21 19:38:10 -0300

    start project

---
+++ frontend/src/pages/MainScreen/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./style.css";
+
+export default function MainScreen(){
+    return(
+        <div>
+            <h1>Main Screen</h1>
+        </div>
+    );
+}
